0215a2b39313acee2c043a8e592af0b5ca83c910
Amra Puzic <amraalic1993@gmail.com>
2020-10-11 18:43:14 +0200

    002 - Add Ship, implement ship movement.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion"""
+	def __init__(self):
+		self.screen_width = 800
+		self.screen_hight = 500
+		self.bg_color = (230, 230, 230)
+
+		self.ship_speed = 0.5
+
