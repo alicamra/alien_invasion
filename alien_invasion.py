ab0fa8c400d7c56b8fdf4bb433e6b604549b3ac6
Amra Puzic <amraalic1993@gmail.com>
2020-10-15 22:38:05 +0200

    005 - Add shooting functionality. Limit number of ships.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,15 @@
 import sys
+from time import sleep
+
 import pygame
+
+from alien import Alien
+from bullet import Bullet
+from game_stats import GameStats
 from settings import Settings
 from ship import Ship
-from bullet import Bullet
-from alien import Alien
+
+
 
 class AlienInvasion:
 	""" Overall class to manage game assets and behaviour """
@@ -18,8 +24,10 @@ class AlienInvasion:
 		#self.settings.screen_height = self.screen.get_rect().height
 
 		self.screen = pygame.display.set_mode(
-			(self.settings.screen_width, self.settings.screen_hight))
+			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
+
+		self.stats = GameStats(self)
 
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
@@ -31,9 +39,14 @@ class AlienInvasion:
 		"""Start the main loop for the game"""
 		while True:
 			self._check_events()
-			self.ship.update()
-			self._update_bullets()
-			self._update_screen()	
+
+			if self.stats.game_active:
+				self.ship.update()
+				self._update_bullets()
+				self._update_aliens()
+			
+			self._update_screen()
+
 
 	def _check_events(self):
 		"""Respond to keypresses and mouse events"""
@@ -75,18 +88,79 @@ class AlienInvasion:
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
 
+		self._check_bullet_alien_collision()		
+	
+	def _check_bullet_alien_collision(self):
+		collisions = pygame.sprite.groupcollide(
+			self.bullets, self.aliens, True, True)
+
+		if not self.aliens:
+			self.bullets.empty()
+			self._create_fleet()
+
 	def _create_fleet(self):
 		alien = Alien(self)
 		alien_width = alien.rect.width
 		available_space_x = self.settings.screen_width - (2*alien_width)
 		number_aliens_x = available_space_x//(2*alien_width)
 
-		for alien_number in range(number_aliens_x):
-			alien = Alien(self)
-			alien.x = alien_width + 2*alien_width*alien_number
-			alien.rect.x = alien.x
-			self.aliens.add(alien)
+		ship_height = self.ship.rect.height
+		alien_height = alien.rect.height
+		available_space_y = self.settings.screen_height - (3*alien_height) - ship_height
+		number_rows = available_space_y//(2*alien_height)
+		
+		for row_number in range(number_rows):
+			for alien_number in range(number_aliens_x):
+				self._create_alien(alien_number, row_number)
 
+	def _create_alien(self, alien_number, row_number):
+		alien = Alien(self)
+		alien_width, alien_height = alien.rect.size
+		alien.x = alien_width + 2*alien_width*alien_number
+		alien.rect.x = alien.x
+		alien.rect.y = alien.rect.height + 2*alien.rect.height*row_number
+		self.aliens.add(alien)
+
+	def _change_fleet_direction(self):
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction *= -1
+
+	def _check_fleet_edges(self):
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _update_aliens(self):
+		"""Update the position of all aliens in the fleet"""
+		self._check_fleet_edges()
+		self.aliens.update()
+
+		if pygame.sprite.spritecollideany(self.ship, self.aliens):
+			self._ship_hit()
+
+		self._check_aliens_bottom()
+
+	def _ship_hit(self):
+		if self.stats.ships_left > 0:
+			self.stats.ships_left -=1
+			self.aliens.empty()
+			self.bullets.empty()
+
+			self._create_fleet()
+			self.ship.center_ship()
+
+			sleep(5)
+		else:
+			self.stats.game_active = False
+
+	def _check_aliens_bottom(self):
+		screen_rect = self.screen.get_rect()
+		for alien in self.aliens.sprites():
+			if alien.rect.bottom >= screen_rect.bottom:
+				self._ship_hit()
+				break
 
 	def _update_screen(self):
 		self.screen.fill(self.settings.bg_color)
--- settings.py
+++ settings.py
@@ -2,13 +2,19 @@ class Settings:
 	"""A class to store all settings for Alien Invasion"""
 	def __init__(self):
 		self.screen_width = 800
-		self.screen_hight = 500
+		self.screen_height = 500
 		self.bg_color = (230, 230, 230)
 
 		self.ship_speed = 0.5
 
-		self.bullet_width = 3
+		self.bullet_width = 10
 		self.bullet_height = 15
-		self.bullet_speed =0.15
-		self.bullet_color = (60, 60, 60)
+		self.bullet_speed =0.7
+		self.bullet_color = (160, 60, 60)
 		self.bullet_limit = 10
+
+		self.alien_speed = 0.5
+		self.fleet_drop_speed = 10
+		self.fleet_direction = 1
+
+		self.ship_limit = 3
